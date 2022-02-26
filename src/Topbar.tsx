014f1e205ad039c07524ad4ec58340b95887c580
Mihnea Buzatu <mihnea.buzatu@metaminds.com>
2022-02-26 12:52:19 +0200

    small refactor

--- src/Topbar.tsx
+++ src/Topbar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Box, Button, Heading, Anchor } from 'grommet';
-import { CaretNext } from 'grommet-icons'
+import { FormNext } from 'grommet-icons'
 
 type Handler = () => void
 type ButtonHandlers = {
@@ -8,6 +8,21 @@ type ButtonHandlers = {
   onRegister: Handler,
   goHome: Handler,
 } 
+
+function Bar(props: any) {
+  return (
+      <Box
+        direction="row"
+        align="center"
+        justify="between"
+        background="gray"
+        pad={{ left: "medium", right: "medium", vertical: "small" }}
+        elevation="medium"
+        style={{ zIndex: "1" }}
+        {...props}
+      />
+  );
+}
 
 class Topbar extends React.Component<ButtonHandlers> {
   onLogin:    Handler;
@@ -23,31 +38,21 @@ class Topbar extends React.Component<ButtonHandlers> {
 
   render(): React.ReactNode {
     return (
-      <Box
-        direction="row"
-        align="center"
-        justify="between"
-        background="gray"
-        pad={{ left: "medium", right: "medium", vertical: "small" }}
-        elevation="medium"
-        style={{ zIndex: "1" }}
-      >
+      <Bar> 
         <Anchor color="cyan">
-          <Heading 
-            level="2"
-            margin="none"
-            color="cyan" 
-            onClick={this.goHome}
-          >
-            FriedLiver
-          </Heading>
+          <Box direction="row" align="center">
+            <FormNext color="purple" size="large"/>
+            <Heading level="2" margin="none" color="cyan" onClick={this.goHome}>
+              FriedLiver
+            </Heading>
+          </Box>
         </Anchor>
         <div>
           <Button label=" Login " color="cyan" onClick={this.onLogin} />
           <a> </a>
           <Button label="Register" color="purple" onClick={this.onRegister}/>
         </div> 
-      </Box>
+      </Bar>
     );
   }
 }
